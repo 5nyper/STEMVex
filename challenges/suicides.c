b57f77e1e318f8387e3e8d9d88e5a49a7a9b57a7
Sinatra <Vikaton@users.noreply.github.com>
2015-11-16 10:36:56 -0500

    Update suicides.c

--- challenges/suicides.c
+++ challenges/suicides.c
@@ -10,3 +10,15 @@ task main()
 		moveBackward(i);
 	}
 }
+
+void moveForward(int i) {
+	motor[port1] = 100;
+	motor[port10] = 100;
+	wait1Msec(i);
+}
+
+void moveBackward(int i) {
+	motor[port1] = -100;
+	motor[port10] = -100;
+	wait1Msec(i);
+}
