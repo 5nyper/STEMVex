9175e94efcac2fb20d5d637e3ded86f837ba1c4e
Sinatra <Vikaton@users.noreply.github.com>
2015-11-17 10:24:12 -0500

    Update challenge001.c

--- challenges/challenge001.c
+++ challenges/challenge001.c
@@ -14,15 +14,3 @@ task main()
 		moveBackward(i);
 	}
 }
-
-void moveForward(int i) {
-	motor[port1] = 100;
-	motor[port10] = 100;
-	wait1Msec(i);
-}
-
-void moveBackward(int i) {
-	motor[port1] = -100;
-	motor[port10] = -100;
-	wait1Msec(i);
-}
