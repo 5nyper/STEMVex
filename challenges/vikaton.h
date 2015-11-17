28845b870977f216d0c87e216865fb731704e92f
Sinatra <Vikaton@users.noreply.github.com>
2015-11-17 10:23:56 -0500

    Update vikaton.h

--- challenges/vikaton.h
+++ challenges/vikaton.h
@@ -1,7 +1,29 @@
 #ifndef VIKATON_H_   /* Include guard */
 #define VIKATON_H_
 
+// prototypes
 void moveForward(int i);
 void moveBackward(int i);
+void turnX(int left, int right, int i);
+
+// function bodies (ROBOTC has no compiling options, 
+// so I had to put both prototypes and function bodies in the same file
+void moveForward(int i) {
+	motor[port1] = 100;
+	motor[port10] = 100;
+	wait1Msec(i);
+}
+
+void moveBackward(int i) {
+	motor[port1] = -100;
+	motor[port10] = -100;
+	wait1Msec(i);
+}
+
+void turnX(int left, int right, int i) {
+	motor[port1] = left;
+	motor[port10] = right;
+	wait1Msec(i);
+}
 
 #endif
